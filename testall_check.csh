79898bed7c47884c8f4e6ebad6b1c2be0bc3de50
joh33-wmko <jhayashi@keck.hawaii.edu>
2024-03-19 18:21:04 -1000

    adds the following: o multiple email recipients for cmd line o subprocess call response to message o uptime report

--- testall_check.csh
+++ testall_check.csh
@@ -1,5 +1,6 @@
 #!/usr/bin/csh
 
 source ~/.cshrc
+#./testall_check.py $*
 /home/filer2/jhayashi/kroot/src/util/testall/testall_check.py $* # remove full path for rel
 
--- testall_check.py
+++ testall_check.py
@@ -1,6 +1,23 @@
 #!/usr/local/anaconda/bin/python
 
-# Uses TestAll.py to determine if there are any current errors for input instrument
+# testall_check.py:
+#   is called by testall_check.csh in crontab on vm-nires
+#   and calls testAll.py to determine if there are any current errors for default instrument
+
+# ToDo 3/19/24 (NIRES):
+# x- add uptime to testall without IDL error (or will trigger email every 5 min when no actual error)
+# x- fix IDL_DIR error ssh'ing to niresserver2
+#   - fixed .cshrc on niresserver2; awaiting Percy's ok to keep (restore) it
+#   x- add uptime results to message var
+# x- add Percy to email list in cron
+#   x- restore send_email() call logic
+# - split testall into instruments and telescpe
+#   - verify terminology and files with Jeff
+# - restore is_instrument_available()
+# - restore is_instrument_scheduled()
+#   date = datetime.strftime(datetime.now().date(), '%Y-%m-%d')   # added
+#   print(date)
+# - restore error for uptime MISSING INFO (to reduce email)?
 
 import argparse
 from datetime import datetime
@@ -12,30 +29,35 @@ from email.mime.text import MIMEText
 import socket
 from urllib.request import urlopen
 import json
+from subprocess import Popen, PIPE
+import pdb
 
-def send_email(message, error):
+def send_email(recipients, message, error):
     errorMsg = 'ERROR: ' if error == 1 else ''
     msg = MIMEText(message)
     msg['Subject'] = f"{errorMsg}{instrument.upper()} RTI Alarm ({socket.gethostname()})"
-    msg['To'] = email
+    msg['To'] = recipients
     msg['From'] = 'koaadmin@keck.hawaii.edu'
     s = smtplib.SMTP('localhost')
     s.send_message(msg)
     s.quit()
 
+def get_uptime(server_name):
+    uptime_cmd = ["ssh", server_name, " uptime"]
+    uptime = subprocess.check_output(uptime_cmd)
+    uptime = uptime.decode('utf-8')
+    uptime = uptime.replace('IDL_DIR: Undefined variable.','')
+    uptime = ' '.join((uptime.split("  ")[1:3])).replace(",","")
+    #if 'ERROR' in uptime:
+    #    return f'uptime not available for {server_name}'
+    return uptime
+
+
 parser = argparse.ArgumentParser()
-#parser.add_argument('instrument', type=str, help='Instrument name')
-#parser.add_argument('--date', type=str, default=datetime.now().strftime('%Y-%m-%d'), help='Specific HST date to search')
 parser.add_argument('--email', type=str, default=None, help='Email results to address')
 args = parser.parse_args()
-#instrument = args.instrument.lower()
 instrument = os.environ['INSTRUMENT']
-#date = args.date
-email = args.email
-
-date = datetime.strftime(datetime.now().date(), '%Y-%m-%d')   # added
-#print(date)
-print(email)
+recipients = args.email
 
 #testallDir = '/kroot/rel/default/data'
 testallDir = '/home/filer2/jhayashi/kroot/src/util/testall/data'
@@ -43,29 +65,56 @@ testallDir = '/home/filer2/jhayashi/kroot/src/util/testall/data'
 defaultFile = f'{testallDir}/testall-{instrument}.yaml'
 #defaultFile = f'{testallDir}/testall-nires.yaml'
 
-#print(testallFile)
-
 error = 0
-#if not os.path.isfile(testallFile):
-    #error = 1
-    #message = f'{testallFile} does not exist'
+uptime_msg = ''
+if instrument.lower() == 'nires':
+    uptime1 = get_uptime('niresserver1')
+    uptime_msg  = f"\nniresserver1's uptime: "
+    uptime_ck = uptime1.split()
+    if 'up' in uptime_ck:
+        #uptime_msg  = f"\nniresserver1's uptime: {uptime1}\n"
+        uptime_msg  += f"{uptime1}\n"
+    else:
+        uptime_msg  += f"{uptime1}                           *** MISSING INFO ***\n"
+        #error = 1                                           # restore if desired to reduce error emails
+
+    uptime2 = get_uptime('niresserver2')
+    uptime_msg += f"niresserver2's uptime: {uptime2}\n\n"
+    print(uptime_msg)
+
+message = uptime_msg
+
+#tbd
+#if instrument.lower() == 'lris':
+#    uptime1 = get_uptime('lris-red')
+#    print(f"uptime for lris-red: {uptime1}")
+#    uptime2 = get_uptime('lris-blue')
+#    print(f"uptime for lris-blue: {uptime2}")
+
+#error = 0
+sub_msg = ''
 if not os.path.isfile(defaultFile):
     error = 1
-    message = f'{defaultFile} does not exist'
+    sub_msg = f'{defaultFile} does not exist'
 else:
     #cmd = ['/kroot/rel/default/bin/testall.py']
+    #cmd = ['/kroot/rel/default/bin/testall.py', '--datadir', testallDir]
     cmd = ['/home/filer2/jhayashi/kroot/src/util/testall/testall.py', '--datadir', testallDir]
-    message = subprocess.check_output(cmd)
-    message = message.decode('utf-8')
-    if 'ERROR' in message:
+    sub_msg = subprocess.check_output(cmd)
+    sub_msg = sub_msg.decode('utf-8')
+    if 'ERROR' in sub_msg:
         error = 1
-        message = f'**** ERROR ****\n\n{message}'
+        sub_msg = f'**** ERROR ****\n\n{sub_msg}'
 
-#if (error and is_instrument_available() and email):
-#    send_email(message, error)
+message += sub_msg
 
-if email != None:
-    send_email(message, error)
+#send_email(recipients, message, error)   # testing...
+
+if (recipients and error):
+   send_email(recipients, message, error)
+
+##if (error and is_instrument_available() and email):
+##    send_email(recipients, message, error)
 
 print()
 print(message)
